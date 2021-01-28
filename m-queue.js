aedd355e9e84a66eb9d26b479d03270aa75035f9
Michael I Bell <dr.m.i.bell@gmail.com>
2021-01-28 13:57:42 -0500

    Version 0.2.1

--- m-queue.js
+++ m-queue.js
@@ -57,7 +57,7 @@ module.exports = function(RED) {
         var keepNewestDefault = node.keepNewestDefault
         var maxSizeDefault = node.maxSizeDefault
         var protectDefault = node.protectDefault
-
+        // Definitions
         let qArrayInit = [
             {
                 qName: defaultQueue,
@@ -67,13 +67,36 @@ module.exports = function(RED) {
                 keepNewest: keepNewestDefault,
                 protect: true   // cannot change
             }]
+        function showStatus(qArray) {
+            let n = 0
+            for (i = 0; i < qArray.length; i++) {
+                n = n + qArray[i].msgQ.length
+            }
+            node.status({text:n + ' messages, ' + qArray.length + ' queues'})
+            return
+        }
+        function findQueue(qSelect,qArray){
+            for (i = 0; i < qArray.length; i++) {
+                if (qArray[i].qName == qSelect) {
+                    return i
+                }
+            }
+            return -1
+        }
+        function enqueue(msg,Q) {
+            if (!Q.paused) {    // queue not paused
+                if (Q.msgQ.length < Q.maxSize) {    // queue not full
+                    Q.msgQ.push(msg)    // queue message
+                } else if (Q.keepNewest) {  // keep newest, otherwise drop
+                    Q.msgQ.push(msg)
+                    Q.msgQ.shift()
+                }
+            }
+        }
+        // Startup
         let qArray = persist ? context.get('qArray',storeName) : qArrayInit
         qArray = qArray || qArrayInit
-        let n = 0   // show status
-        for (i = 0; i < qArray.length; i++) {
-            n = n + qArray[i].msgQ.length
-            }
-        node.status({text:n + ' messages, ' + qArray.length + ' queues'})
+        showStatus(qArray)
         context.set('qArray', qArray,storeName)
         // Process inputs
         node.on('input', function(msg) {
@@ -81,65 +104,43 @@ module.exports = function(RED) {
             let qSelect = msg[queueSelect]
             let Q = {}
             let qIndex = 0
-            let qExists = true
-            if (qSelect === allQueues) {
-                first = 0
-                last = qArray.length
-            } else if (qSelect == undefined || qSelect == '') { // qSelect undefined?
-                first =0
-                last = 1
-            } else {    // queue exists?
-                qExists = false
-                for (i = 0; i < qArray.length; i++) {
-                    if (qArray[i].qName == qSelect) {
-                        qExists = true
-                        qIndex = i
-                    }
-                }
-                if (qExists) {
-                    first = qIndex
-                    last = qIndex + 1
-                } else {
+            if (typeof qSelect === 'undefined' || qSelect === '') { // qSelect undefined?
+                qSelect =  defaultQueue
+            }            
+            if (msg[controlFlag]) {  // execute command
+                if (qSelect === allQueues){
                     first = 0
-                    last = 1
+                    last = qArray.length
+                } else {
+                    first = findQueue(qSelect,qArray)
+                    last = first + 1
                 }
-            }
-            for (qIndex = first; qIndex < last; qIndex++) {
-                if (msg[controlFlag]) {  // execute command
-                    if (!qExists) {
-                        node.warn('Invalid command ignored: queue does not exist.')
-                        return
-                    }
-                    Q = qArray[qIndex]
-                    switch (msg.payload) {
+                if (first < 0) {   // debug: consider createCmd
+                    node.warn('Invalid command ignored: queue does not exist.')
+                    return
+                }
+                for (i = first; i < last; i++) {
+                    Q = qArray[i]
+                    switch (msg.payload.toString().toLowerCase()) {
                         case node.triggerCmd:
                             if (Q.paused) {break}
-                            node.send (Q.msgQ.shift())
-//                            if (qIndex != 0 && !Q.protect && Q.msgQ.length === 0) {
-                            if (Q.msgQ.length === 0 && !Q.protect) {
-                                qArray.splice(qIndex,1)  // delete empty queue
-                            }
+                            node.send(Q.msgQ.shift())
                             break
-                        case node.peekCmd:    // works when queue is paused
-                            node.send(RED.util.cloneMessage(Q.msgQ[0]));
+                        case node.peekCmd:
+                            node.send(RED.util.cloneMessage(Q.msgQ[0]))
                             break
                         case node.dropCmd:
                             if (Q.paused) {break}
                             Q.msgQ.shift()
-                            if (Q.msgQ.length === 0 && !Q.protect) {
-                                qArray.splice(qIndex,1)  // delete empty queue
-                            }
                             break
                         case node.flushCmd:
                             if (Q.paused) {break}
                             node.send([Q.msgQ])
+                            Q.msgQ = []                            
+                            break
                         case node.resetCmd:
                             if (Q.paused) {break}
-                            if (!Q.protect) {  // delete queue
-                                qArray.splice(qIndex,1)
-                            } else {    // clear message queue
-                                Q.msgQ = []
-                            }
+                            Q.msgQ = []
                             break
                         case node.resumeCmd:
                             Q.paused = false
@@ -149,72 +150,71 @@ module.exports = function(RED) {
                             break
                         case node.maximumCmd:
                             let newMax = msg[newValue]
-                            if (newMax == undefined) {
+                            if (typeof newMax === 'undefined') {
                                 Q.maxSize = maxSizeDefault
                             } else if (newMax <= 0) {
                                 Q.maxSize = Infinity
                             } else {
-                                Q.maxSize = newMax 
+                                Q.maxSize = newMax
                             }
-                            context.set('qArray',qArray,storeName)
                             break
                         case node.newestCmd:
                             let newKeep = msg[newValue]
-                            if (newKeep == undefined) {
+                            if (typeof newKeep === 'undefined') {
                                 newKeep = keepNewestDefault
                             }
                             Q.keepNewest = newKeep
-                            context.set('qArray',qArray,storeName)
-                        break
-                        case node.statusCmd:
-                            node.send([null,{payload: Q}])
-                        break
+                            break
                         case node.protectCmd:
-                            if (qIndex === 0) {break}
+                            if (Q.qName === defaultQueue) {break}
                             let newProtect = msg[newValue]
-                            if (newProtect == undefined) {
+                            if (typeof newProtect === 'undefined') {
                                 newProtect = protectDefault
                             }
                             Q.protect = newProtect
-                            context.set('qArray',qArray,storeName)
-                        break
+                            break
                         case node.deleteCmd:
                             if (Q.paused) {break}
-                            if (qIndex != 0) {  // delete queue
-                                qArray.splice(qIndex,1)
-                            }
-                        break
+                            if (Q.qName != defaultQueue)
+                                Q.protect = false
+                                Q.msgQ = []
+                            break
+                        case node.statusCmd:
+                            node.send([null,{payload: qArray[i]}])
+                            break
                         default:
-                            node.warn('Invalid command ignored');
-                        }
-                    } else {    // queue message
-                        if (!qExists) {  // if queue does not exist, create new one
-                            qArray.push({
-                                qName: qSelect,
-                                msgQ: [msg],
-                                maxSize: maxSizeDefault,
-                                paused: false,
-                                keepNewest: keepNewestDefault,
-                                protect: protectDefault
-                            })
-                        } else {  // use existing queue
-                            Q = qArray[qIndex]
-                            if (!Q.paused) {   // queue not paused
-                                if (Q.msgQ.length < Q.maxSize) {  // queue not full
-                                    Q.msgQ.push(msg)
-                                } else if (Q.keepNewest) { // keep newest, drop oldest
-                                    Q.msgQ.push(msg);
-                                    Q.msgQ.queue.shift();
-                                }
-                            }
-                        }
+                            node.warn('Invalid command ignored')
                     }
                 }
-            let n = 0   // show status
-            for (i = 0; i < qArray.length; i++) {
-                n = n + qArray[i].msgQ.length
+                let qArrayTemp = []    // delete empty queues
+                for (i = 0; i < qArray.length; i++){
+                    if (qArray[i].protect || qArray[i].msgQ.length > 0) {
+                        qArrayTemp.push(qArray[i])
+                    }
                 }
-            node.status({text:n + ' messages, ' + qArray.length + ' queues'})
+                qArray = qArrayTemp
+            } else {    // queue message
+                if (qSelect === allQueues) {
+                    for (i = 0; i < qArray.length; i++) {
+                        enqueue(msg,qArray[i])
+                    }
+                } else {
+                    qIndex = findQueue(qSelect,qArray)
+                    if (qIndex < 0) {   // create new queue
+                        qArray.push({
+                            qName: qSelect,
+                            msgQ: [msg],
+                            maxSize: maxSizeDefault,
+                            paused: false,
+                            keepNewest: keepNewestDefault,
+                            protect: protectDefault
+                        })
+                    } else {    // use existing queue
+                        enqueue(msg,qArray[qIndex])
+                    }
+                }
+            }
+            showStatus(qArray)
             context.set('qArray',qArray,storeName)
         })
     }
